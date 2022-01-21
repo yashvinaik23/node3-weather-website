d9af6618dee63b2774da8c3dafa68042d6968537
yashvinaik23 <yashvipnaik2312@gmail.com>
2022-01-21 18:52:43 +0530

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const weather = require("./utils/weather");
 // console.log(__dirname);
 // console.log(path.join(__dirname, "../public"));
 const app = express();
+const port = process.env.PORT || 3000;
 
 ////Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -79,6 +80,6 @@ app.get("*", (req, res) => {
     errorMsg: "404: Page not found",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
